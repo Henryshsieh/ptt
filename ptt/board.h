5256efe3262a4f96e455458529511d74c9def8f7
henryshsieh@gmail.com <henryshsieh@gmail.com>
2021-05-22 15:54:49 +0800

    Create posts on a board

--- ptt/board.h
+++ ptt/board.h
@@ -6,25 +6,81 @@ enum vote
 	UPVOTE,
 	DOWNVOTE
 };
+
 struct Comment
 {
 	bool vote;
 	string message;
 };
+
 struct Post
 {
 public:
 	void setTitle();
-	void setContent();
+	void setContents();
 	void setComment();
 	string title;
-	string content;
+	string contents;
 	vector<Comment> comments;
 };
+
+void Post::setTitle() {
+	cout << "Title: ";
+	getline(cin, title);
+}
+
+void Post::setContents() {
+	string buffer;
+	cout << "Editing:\n *type \"SAVE\" when you finish editing.\n";
+	while (getline(cin, buffer) && buffer != "SAVE") {
+		if (buffer == "SAVE")
+			cout << "contents saved.\n";
+		else
+			contents += buffer + "\n";
+	}
+}
+
+void Post::setComment() {
+	Comment newComment;
+	cout << "leave a comment: ";
+	getline(cin, newComment.message);
+	// TODO: voting mechanism
+	comments.push_back(newComment);
+}
+
 class Board
 {
-	void editBoard();
+public:
 	Board();
+	void editBoard();
 private:
 	vector<Post> posts;
 };
+
+Board::Board() {
+}
+
+void Board::editBoard() {
+	char cmd;
+	while (true) {
+		cout << "Select a mode:\n 'n'ew post\n"
+			" 'e'dit an existing post\n 'd'elete a post\n 'q'uit\n";
+		cin >> cmd;
+		cin.ignore();
+		if (cmd == 'n') {
+			Post newPost;
+			newPost.setTitle();
+			newPost.setContents();
+			newPost.setComment();
+			posts.push_back(newPost);
+		}
+		else if (cmd == 'e') {
+			cout << "Under construction!\n";
+		}
+		else if (cmd == 'd') {
+			cout << "Under construction!\n";
+		}
+		else if (cmd == 'q')
+			return;
+	}
+}
